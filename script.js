362487cf11c377904ff55098fdc74b8bdc69671a
Sutej Jantali <iamsutej@gmail.com>
2024-06-27 23:56:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMobileMenu(){
+	document.getElementById("menu").classList.toggle("active");
+}
+
+function linkedin(){
+	location.href = 'https://www.linkedin.com/in/sutej-r-jantali-859719228';
+}
+
+function leetcode(){
+	location.href = 'https://leetcode.com/u/Sutej_2002/';
+}
+
+function geeksforgeeks(){
+	location.href = 'https://www.geeksforgeeks.org/user/iamsutej/';
+}
+
+function github(){
+	location.href = 'https://github.com/SutejJantali';
+}
+
+function resume(){
+	location.href = 'https://drive.google.com/file/d/1s7AcW10GwzDeLzbY1ZwnD6zgXQ7nwN3p/view?usp=sharing';
+}
